14ecfcdc3aef19f9aa561b49f37ef7607ae5fe01
Logan Kulinski <rashes_lineage02@icloud.com>
2026-01-10 14:32:11 -0600

    JSpecify + Model updates

--- src/main/java/com/cta4j/bus/model/Stop.java
+++ src/main/java/com/cta4j/bus/model/Stop.java
@@ -1,8 +1,13 @@
 package com.cta4j.bus.model;
 
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@NullMarked
+@SuppressWarnings("ConstantConditions")
 public record Stop(
     String id,
 
@@ -12,12 +17,16 @@ public record Stop(
 
     BigDecimal longitude,
 
+    @Nullable
     List<Integer> detoursAdded,
 
+    @Nullable
     List<Integer> detoursRemoved,
 
+    @Nullable
     Integer gtfsSequence,
 
+    @Nullable
     Boolean adaAccessible
 ) {
 }
