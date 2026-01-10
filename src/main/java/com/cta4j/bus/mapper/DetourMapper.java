c0192ec99b739721355d47a0193bbff0c8e936d9
Logan Kulinski <rashes_lineage02@icloud.com>
2026-01-10 15:42:38 -0600

    JSpecify + Model updates

--- src/main/java/com/cta4j/bus/mapper/DetourMapper.java
+++ src/main/java/com/cta4j/bus/mapper/DetourMapper.java
@@ -7,12 +7,13 @@ import com.cta4j.bus.model.DetourRouteDirection;
 import org.jetbrains.annotations.ApiStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper
 @ApiStatus.Internal
 public interface DetourMapper {
     @Mapping(source = "ver", target = "version")
-    @Mapping(source = "st", target = "active")
+    @Mapping(source = "st", target = "active", qualifiedByName = "mapActive")
     @Mapping(source = "desc", target = "description")
     @Mapping(source = "rtdirs", target = "routeDirections")
     @Mapping(source = "startdt", target = "startTime")
@@ -23,4 +24,9 @@ public interface DetourMapper {
     @Mapping(source = "rt", target = "route")
     @Mapping(source = "dir", target = "direction")
     DetourRouteDirection toDomain(CtaDetoursRouteDirection dto);
+
+    @Named("mapActive")
+    default boolean mapActive(int st) {
+        return st == 1;
+    }
 }
