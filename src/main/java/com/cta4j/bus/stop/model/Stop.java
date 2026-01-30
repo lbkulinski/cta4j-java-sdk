d87c1dfbdfd4f3b90afaf17b867e764f2e88b59a
Logan Kulinski <rashes_lineage02@icloud.com>
2026-01-30 14:24:33 -0600

    JavaDoc comments

--- src/main/java/com/cta4j/bus/stop/model/Stop.java
+++ src/main/java/com/cta4j/bus/stop/model/Stop.java
@@ -11,13 +11,13 @@ import java.util.Objects;
  * Represents a bus stop.
  *
  * @param id the unique identifier of this stop
- * @param name the name of this stop
- * @param latitude
- * @param longitude
- * @param detoursAdded
- * @param detoursRemoved
- * @param gtfsSequence
- * @param adaAccessible
+ * @param name the display name of this stop (e.g. "Madison and Clark")
+ * @param latitude the latitude coordinate of this stop
+ * @param longitude the longitude coordinate of this stop
+ * @param detoursAdded the {@code List} of detour IDs which temporarily add service to this stop
+ * @param detoursRemoved the {@code List} of detour IDs which temporarily remove service from this stop
+ * @param gtfsSequence the GTFS sequence number of this stop, if applicable
+ * @param adaAccessible whether this stop is ADA accessible, if known
  */
 @NullMarked
 public record Stop(
@@ -41,6 +41,20 @@ public record Stop(
     @Nullable
     Boolean adaAccessible
 ) {
+    /**
+     * Constructs a {@code Stop}.
+     *
+     * @param id the unique identifier of the stop
+     * @param name the display name of the stop (e.g. "Madison and Clark")
+     * @param latitude the latitude coordinate of the stop
+     * @param longitude the longitude coordinate of the stop
+     * @param detoursAdded the {@code List} of detour IDs which temporarily add service to the stop
+     * @param detoursRemoved the {@code List} of detour IDs which temporarily remove service from the stop
+     * @param gtfsSequence the GTFS sequence number of the stop, if applicable
+     * @param adaAccessible whether the stop is ADA accessible, if known
+     * @throws NullPointerException if {@code id}, {@code name}, {@code latitude}, or {@code longitude} is
+     * {@code null}, or if any element of {@code detoursAdded} or {@code detoursRemoved} is {@code null}
+     */
     public Stop {
         Objects.requireNonNull(id);
         Objects.requireNonNull(name);
