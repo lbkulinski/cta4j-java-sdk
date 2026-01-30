854243c24fe944b6adf2bc606e67491b13432fb3
Logan Kulinski <rashes_lineage02@icloud.com>
2026-01-30 12:27:27 -0600

    JavaDoc comments

--- src/main/java/com/cta4j/bus/prediction/model/PredictionType.java
+++ src/main/java/com/cta4j/bus/prediction/model/PredictionType.java
@@ -1,7 +1,16 @@
 package com.cta4j.bus.prediction.model;
 
+/**
+ * Represents the type of bus prediction.
+ */
 public enum PredictionType {
+    /**
+     * Indicates an arrival prediction.
+     */
     ARRIVAL,
 
+    /**
+     * Indicates a departure prediction.
+     */
     DEPARTURE
 }
