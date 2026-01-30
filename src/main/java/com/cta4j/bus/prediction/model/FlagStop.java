d13b7241b6510ea7bde96b55ef7e80984e6457aa
Logan Kulinski <rashes_lineage02@icloud.com>
2026-01-30 11:31:23 -0600

    JavaDoc comments

--- src/main/java/com/cta4j/bus/prediction/model/FlagStop.java
+++ src/main/java/com/cta4j/bus/prediction/model/FlagStop.java
@@ -1,12 +1,27 @@
 package com.cta4j.bus.prediction.model;
 
+/**
+ * Represents the flag-stop information for a prediction.
+ */
 public enum FlagStop {
+    /**
+     * Indicates that no flag-stop information is available.
+     */
     UNDEFINED(-1),
 
+    /**
+     * Indicates a normal stop.
+     */
     NORMAL(0),
 
+    /**
+     * Indicates a stop where passengers are both picked up and discharged.
+     */
     PICKUP_AND_DISCHARGE(1),
 
+    /**
+     * Indicates a stop where only discharging of passengers occurs.
+     */
     ONLY_DISCHARGE(2);
 
     private final int code;
@@ -15,6 +30,11 @@ public enum FlagStop {
         this.code = code;
     }
 
+    /**
+     * Gets the code associated with this flag-stop.
+     *
+     * @return the flag-stop code
+     */
     public int getCode() {
         return this.code;
     }
--- src/main/java/com/cta4j/bus/prediction/model/PassengerLoad.java
+++ src/main/java/com/cta4j/bus/prediction/model/PassengerLoad.java
@@ -1,11 +1,26 @@
 package com.cta4j.bus.prediction.model;
 
+/**
+ * Represents the passenger load information for a prediction.
+ */
 public enum PassengerLoad {
+    /**
+     * Indicates that the bus is full.
+     */
     FULL,
 
+    /**
+     * Indicates that the bus is half full.
+     */
     HALF_EMPTY,
 
+    /**
+     * Indicates that the bus is empty.
+     */
     EMPTY,
 
+    /**
+     * Indicates that no passenger load information is available.
+     */
     UNKNOWN
 }
