8a21d1d2892e6e3072808badb45b476776c2beb6
Logan Kulinski <lbkulinski@gmail.com>
2026-06-27 12:14:18 -0500

    Code cleanup

--- src/main/java/com/cta4j/common/geo/GeoConstants.java
+++ src/main/java/com/cta4j/common/geo/GeoConstants.java
@@ -7,15 +7,12 @@ import java.math.BigDecimal;
 @NullMarked
 public final class GeoConstants {
     public static final int MIN_HEADING = 0;
-
     public static final int MAX_HEADING = 359;
 
     public static final BigDecimal MIN_LATITUDE = BigDecimal.valueOf(-90.0);
-
     public static final BigDecimal MAX_LATITUDE = BigDecimal.valueOf(90.0);
 
     public static final BigDecimal MIN_LONGITUDE = BigDecimal.valueOf(-180.0);
-
     public static final BigDecimal MAX_LONGITUDE = BigDecimal.valueOf(180.0);
 
     private GeoConstants() {
