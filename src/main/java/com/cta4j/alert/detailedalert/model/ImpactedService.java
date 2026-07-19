aa7a55ecb48c9a731907dd3e8cc18e886e750247
Logan Kulinski <lbkulinski@gmail.com>
2026-07-19 16:26:14 -0500

    Refactor Javadoc comments for clarity in ImpactedService

--- src/main/java/com/cta4j/alert/detailedalert/model/ImpactedService.java
+++ src/main/java/com/cta4j/alert/detailedalert/model/ImpactedService.java
@@ -10,11 +10,11 @@ import java.util.Objects;
  * Represents a single service - a bus route, train route, train station, or systemwide grouping - impacted by
  * an alert.
  *
- * @param type the type of service this represents
+ * @param type the type of service this service represents
  * @param name the name of this service (e.g., "Clark", "Red Line", "Jackson", "All Bus Routes")
  * @param serviceId the identifier of this service; matches GTFS route or station IDs, except for systemwide groupings,
  *                  which use a fixed identifier instead (e.g., "22", "Red", "Systemwide")
- * @param color the color of this service used in maps; length and casing vary (e.g., "059", "565a5c")
+ * @param color the color of this service used in maps, as {@code rrggbb} (e.g., "565a5c")
  * @param textColor the suggested color of text displayed against {@code color}; casing varies (e.g., "ffffff",
  *                  "FFFFFF")
  * @param url the URL of this service's page on transitchicago.com
@@ -31,11 +31,11 @@ public record ImpactedService(
     /**
      * Constructs an {@code ImpactedService}.
      *
-     * @param type the type of service the impacted service represents
+     * @param type the type of service the service represents
      * @param name the name of the service (e.g., "Clark", "Red Line", "Jackson", "All Bus Routes")
      * @param serviceId the identifier of the service; matches GTFS route or station IDs, except for systemwide
      *                  groupings, which use a fixed identifier instead (e.g., "22", "Red", "Systemwide")
-     * @param color the color of the service used in maps; length and casing vary (e.g., "059", "565a5c")
+     * @param color the color of the service used in maps, as {@code rrggbb} (e.g., "565a5c")
      * @param textColor the suggested color of text displayed against {@code color}; casing varies (e.g., "ffffff",
      *                  "FFFFFF")
      * @param url the URL of the service's page on transitchicago.com
